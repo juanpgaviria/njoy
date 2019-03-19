d795e0055a9c352077d556a9eec2d1f09690c993
jeferbc <jeffe.bernal@gmail.com>
2019-03-19 18:51:34 -0500

    Create codeclimate, rubocop, and simplecov
    Fixes #3

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,10 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter 'spec'
+end
+
 require 'capybara/rails'
-require "chromedriver-helper"
+require 'chromedriver-helper'
 require 'database_cleaner'
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
@@ -7,7 +12,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
