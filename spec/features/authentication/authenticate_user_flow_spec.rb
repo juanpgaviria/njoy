957661d524a0990513b278d5f1f865ddd986a464
jeferbc <jeffe.bernal@gmail.com>
2019-04-03 15:33:30 -0500

    add bootstrap and friendly ux

--- spec/features/authentication/authenticate_user_flow_spec.rb
+++ spec/features/authentication/authenticate_user_flow_spec.rb
@@ -4,14 +4,19 @@ describe 'User signin process', type: :feature, js: true do
   let!(:user) { create(:user, email: 'user@example.com', password: 'password') }
 
   it 'sign in and sign out' do
-    visit '/users/sign_in'
+    visit '/'
+
+    click_link 'Entrar'
+    click_link 'Dueño'
+    expect(page).to have_current_path(new_user_session_path)
 
     within('form#new_user') do
       fill_in 'user_email', with: 'user@example.com'
       fill_in 'user_password', with: 'password'
-      click_button 'Log in'
+      click_button 'Entrar'
     end
     expect(page).to have_current_path(companies_path)
+    click_link user.email.split('@')[0].titleize
     click_link 'Salir'
     expect(page).to have_current_path(root_path)
   end
--- spec/features/authentication/registration_flow_spec.rb
+++ spec/features/authentication/registration_flow_spec.rb
@@ -11,23 +11,15 @@ describe 'User registration', type: :feature, js: true do
     within('.new_user') do
       fill_in 'user_email', with: 'njoy@gmail.com'
       fill_in 'user_password', with: 'test123'
+      fill_in 'user_password_confirmation', with: 'test1234'
+      click_button 'Registrarse'
+      expect(page).to have_css('#error_explanation')
+      fill_in 'user_password', with: 'test123'
       fill_in 'user_password_confirmation', with: 'test123'
-      click_button 'Sign up'
+      click_button 'Registrarse'
     end
 
     expect(page).to have_current_path(companies_path)
     expect(User.count).to eq 1
-  end
-
-  it 'fail' do
-    click_link 'Registrate'
-
-    within('.new_user') do
-      fill_in 'user_email', with: 'njoy@gmail.com'
-      fill_in 'user_password', with: 'test1234'
-      fill_in 'user_password_confirmation', with: 'test123'
-      click_button 'Sign up'
-    end
-    expect(User.count).to eq 0
   end
 end
